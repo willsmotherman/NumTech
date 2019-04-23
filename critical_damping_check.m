b9a91a4b95cd0597d02b4f0532712629cd7d0010
Yerboyo <49619982+Yerboyo@users.noreply.github.com>
2019-04-23 18:39:31 -0400

    Update critical_damping_check.m

--- critical_damping_check.m
+++ critical_damping_check.m
@@ -1,19 +1,28 @@
-function [outputArg1] = critical_damping_check(inputArg1)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
+function [cc] = critical_damping_check([initial displacement],m,k)
+% Critical Damping Check; the last part of the code
+%   Detailed explanation
+
 criteria = 0;
-k = 0;
+n = 0;
+tau = 2*pi*sqrt(m/k); %solve for period of oscillation
+tspan = [0 tau]; %tspan is the length of time for an oscillation- the period!
 %y0 = [initial displacement from equilibrium... input?]
+
 while criteria = 0
-    k = k + 1;
-    tau = 2*pi*sqrt(m/k); %solve for period of oscillation
-    tspan = [0 tau]; %tspan is the length of time for an oscillation- the period!
-    [t,y] = ode23(ODEFUN,tspan,y0);
+    n = n + 1;
+    [t,y] = ode23(solode,tspan,y0);
     time(k) = t;
     response(k) = y;
-    if %close enough to critical damping; velocity at equilibrium is less than a certain portion of initial velocity
-        critera = 1;
+    if y == 0 %close enough to critical damping; displacement after reaching equilibrium does not go too far past zero
+        while critera = 0
+            n = n + 1;
+            [t,y] = ode23(solode,tspan,y0);
+            time(n) = t;
+            response(n) = y;
+            if y > abs([portion of initial displacement])
+                criteria = 1;
+            end
+        end
     end
 end
 %plot(time,response);
