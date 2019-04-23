be13d8bee6a917e491361bc4b9de053bded2d297
Yerboyo <49619982+Yerboyo@users.noreply.github.com>
2019-04-23 19:04:34 -0400

    Update critical_damping_check.m

--- critical_damping_check.m
+++ critical_damping_check.m
@@ -1,19 +1,27 @@
-function [cc] = critical_damping_check([initial displacement],m,k)
+function [cc] = critical_damping_check(y0,m,k)
 % Critical Damping Check; the last part of the code
 %   Detailed explanation
 
 criteria = 0;
 n = 0;
 tau = 2*pi*sqrt(m/k); %solve for period of oscillation
-tspan = [0 tau]; %tspan is the length of time for an oscillation- the period!
-%y0 = [initial displacement from equilibrium... input?]
+tspan = [0 (0.5*tau)]; %tspan is the length of time for half of an oscillation- half of the period!
+
+phi = (1+sqrt(5))/2;
+cL = 0;
+cU = 2;
 
 while criteria = 0
+    d = (phi-1)*(cU-cL);
+    c1 = cL + d;
+    c2 = cU - d;
     n = n + 1;
-    [t,y] = ode23(solode,tspan,y0);
-    time(k) = t;
-    response(k) = y;
-    if y == 0 %close enough to critical damping; displacement after reaching equilibrium does not go too far past zero
+    [t1,y1] = ode23([some function],tspan,y0);
+    [t2,y2] = ode23([some other function],tspan,y0);
+    response1(n) = y1;
+    response2(n) = y2;
+    
+    if y1 < [some y] || y2 < [some y] %close enough to critical damping; displacement after reaching equilibrium does not go too far past zero
         while critera = 0
             n = n + 1;
             [t,y] = ode23(solode,tspan,y0);
