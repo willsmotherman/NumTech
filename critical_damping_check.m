3bf07793bfa256f066a5729ce69541f85f0b8e09
Yerboyo <49619982+Yerboyo@users.noreply.github.com>
2019-04-25 22:02:36 -0400

    Update critical_damping_check.m

--- critical_damping_check.m
+++ critical_damping_check.m
@@ -68,12 +68,12 @@ while criteria == 0
     else
         y_2 = y2(TF2first,2);
     end
-    if abs(y_1-equi) < tol %close enough to critical damping
+    if abs(y_1+equi) < tol %close enough to critical damping
          criteria = 1; %end the while loop; c1 is the critical damping
-    elseif abs(y_2-equi) < tol
+    elseif abs(y_2+equi) < tol
         criteria = 2; %end the while loop; c2 is the critical damping
     end
-    if abs(y_1-equi) < abs(y_2-equi)
+    if abs(y_1+equi) < abs(y_2+equi)
         cL = c2;
     else
         cU = c1;
